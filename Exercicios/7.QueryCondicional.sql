72581c8f6d752692a65b1b41c7e142b540920114
WendelPassos <wendeleduardo137@gmail.com>
2021-12-15 22:38:51 -0300

    Update

---
+++ Exercicios/7.QueryCondicional.sql
@@ -0,0 +1,3 @@
+--Qual seria o comando SQL para selecionar todos os itens de notas fiscais, cuja quantidade seja maior que 60 e preço menor ou igual a 3?
+
+select * from itens_notas_fiscais where quantidade > 60 and preco <= 3;
---
+++ Exercicios/8.QueryLike.sql
@@ -0,0 +1,3 @@
+--Quantos clientes possuem o último sobrenome Mattos?
+
+select * from tabela_de_clientes where nome like '%Mattos'
