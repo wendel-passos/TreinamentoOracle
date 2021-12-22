b2bb8bdda8489497b276749f479869907ad3603e
WendelPassos <wendeleduardo137@gmail.com>
2021-12-21 22:19:57 -0300

    Update 18.QueryPipe.sq.sql

--- Exercicios/18.QueryPipe.sq.sql
+++ Exercicios/18.QueryPipe.sq.sql
@@ -1,3 +1,3 @@
 --Crie uma consulta que mostre o nome e a idade dos clientes.
 
-select nome, months_between (sysdate, data_de_nascimento)/ 12 AS IDADE from ALURA.tabela_de_clientes
+select nome, SUBSTR (months_between (sysdate, data_de_nascimento)/ 12,1,2) AS IDADE from ALURA.tabela_de_clientes
